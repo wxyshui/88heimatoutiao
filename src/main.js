7eeda9e2d089f333c710932214ca94a25ba90115
wxyshui <993691806@qq.com>
2019-11-12 17:59:36 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.config.productionTip = false
+import './styles/index.less'
 Vue.use(ElementUI)
-
+Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
