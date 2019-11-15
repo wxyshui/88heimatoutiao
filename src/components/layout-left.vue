54498a273a007b2ffbe9a57af5da0de321c2af33
wxyshui <993691806@qq.com>
2019-11-15 20:38:39 +0800

    路由嵌套与路由页面和导航菜单对应

--- src/components/layout-left.vue
+++ src/components/layout-left.vue
@@ -1,15 +1,15 @@
 <template>
-  <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" style="width:216px">
+  <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" style="width:216px" router>
       <div class='menu-title'>
           <img src="../assets/img/logo_admin.png" alt="" >
       </div>
-    <el-menu-item index="1">首页</el-menu-item>
-    <el-submenu index="2">
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-submenu>
       <template slot='title'>内容管理</template>
-      <el-menu-item>发布文章</el-menu-item>
-      <el-menu-item>内容列表</el-menu-item>
-      <el-menu-item>评论列表</el-menu-item>
-      <el-menu-item>素材管理</el-menu-item>
+      <el-menu-item index="/pulish">发布文章</el-menu-item>
+      <el-menu-item index='/article'>内容列表</el-menu-item>
+      <el-menu-item  index='/comment'>评论列表</el-menu-item>
+      <el-menu-item index='/image'>素材管理</el-menu-item>
     </el-submenu>
     <el-submenu index="3">
       <template slot='title'>粉丝管理</template>
@@ -18,7 +18,7 @@
       <el-menu-item>粉丝画像</el-menu-item>
       <el-menu-item>粉丝列表</el-menu-item>
     </el-submenu>
-    <el-menu-item index="4">账户信息</el-menu-item>
+    <el-menu-item index="/account">账户信息</el-menu-item>
   </el-menu>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Layout from '../views/layout'
+// 登录组件
 import Login from '../views/login'
+// 结构组件
+import Layout from '../views/layout'
+// 首页组件
+import Home from '@/views/home'
+// 发表文章组件
+import Pulish from '@/views/publish'
+// 文章列表组件
+import Article from '@/views/article'
+// 评论列表组件
+import Comment from '@/views/comment'
+// 账户设置组件
+import Account from '@/views/account'
+// 素材管理组件
+import Image from '@/views/images'
+// @  是  vuecli 中提供的一种特殊路径规则  他直接指向  src 目录
 
 Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: Layout
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Home
+      },
+      {
+        path: '/pulish',
+        component: Pulish
+      },
+      {
+        path: '/article',
+        component: Article
+      },
+      {
+        path: '/comment',
+        component: Comment
+      },
+      {
+        path: '/account',
+        component: Account
+      },
+      {
+        path: '/image',
+        component: Image
+      }
+    ]
   }, {
     path: '/login',
     component: Login
